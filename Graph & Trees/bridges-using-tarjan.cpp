22ab5d4c9337c576ae87409ed02da931b52afd89
Seif <seif1782006@gmail.com>
2026-05-16 08:09:55 +0300

    modified bridges

--- Graph & Trees/bridges-using-tarjan.cpp
+++ Graph & Trees/bridges-using-tarjan.cpp
@@ -1,16 +1,16 @@
-vector<int> adj[N];
-int in[N], low[N], timer=1;
-vector<pair<int,int>> bridges;
+vector<pair<int,int>> adj[N];
+int n, m, timer = 1, in[N], low[N], isBridge[N];
 
-void dfs(int u, int p) {
+void tarjan(int u, int p) {
     in[u] = low[u] = timer++;
-    for (int v : adj[u]) if (v != p) {
-        if (!in[v]) {
-            dfs(v, u);
+    for (const auto& [v, e] : adj[u]) if (v != p) {
+        if (in[v])
+            low[u] = min(low[u], in[v]);
+        else {
+            tarjan(v, u);
             low[u] = min(low[u], low[v]);
             if (low[v] > in[u])
-                bridges.emplace_back(u, v);
+                isBridge[e] = true;
         }
-        else low[u] = min(low[u], in[v]);
     }
 }
